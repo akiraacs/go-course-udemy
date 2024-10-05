295c0fdadf365d3d7aa3f9ea63dba864a700d30c
akiraacs <alfabeto.coradini@hotmail.com>
2024-10-05 18:15:45 -0300

    Exemplo de struct utilizando struct com usuarios

---
+++ struct-example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Struct users
+type user struct {
+	name    string
+	age     int
+	address address
+}
+
+// Struct address of users
+type address struct {
+	street string
+	number int
+	zip    int
+}
+
+func main() {
+
+	user_address := address{street: "Rua colatina", number: 283, zip: 29102841}
+
+	var user1 user
+	user1.name = "Debora"
+	user1.age = 26
+	user1.address = user_address
+
+	user2 := user{name: "Adrian", age: 23, address: user_address}
+
+	fmt.Printf("Users: %v, %v\n", user1, user2)
+}
